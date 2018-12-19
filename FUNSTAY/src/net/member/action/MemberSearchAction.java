4002418d43eb54b026464da4631bc150a77c964f
ITWILL <ITWILL@ITWILLBS02-24>
2018-12-19 20:12:58 +0900

    서치 - 신혜진

--- FUNSTAY/src/net/member/action/MemberSearchAction.java
+++ FUNSTAY/src/net/member/action/MemberSearchAction.java
@@ -2,6 +2,7 @@ package net.member.action;
 
 import java.io.PrintWriter;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -56,20 +57,51 @@ public class MemberSearchAction implements Action{
 		MemberDAO mdd= new MemberDAO();
 
 		int pageSize = 10;
+
+		List a = new ArrayList();
+		List rest = null;
+	    List list = null;
+	    List past = null;
 		Vector vector = mdd.getsearchList(hb, start_date, end_date,num);
 		Vector vector2 = mdd.getsearchList2(hb, num);
 		Vector vector3 = mdd.getsearchList3(hb);
-		List list=(List)vector.get(0);
-	    if(vector.size()==2)
-	    {
+		
+		System.out.println("ddddddddddddddddddddddddddddd");
+		System.out.println(vector.size());
+		int s = vector.size();
+		System.out.println(s);
+	    if(s==2)
+	    {System.out.println("ddddddd1111dddddddddddddddddddddd");
 	    	List avg=(List)vector.get(1);
 	    	session.setAttribute("avg", avg);
 	    }
-
-		List past =(List)vector2.get(0);
-		List rest =(List)vector3.get(0);
+	    else if(vector.size()==1)
+	    {
+	    	list=(List)vector.get(0);
+	    }
+	    else if(vector2.size()==2)
+	    {System.out.println("dddddddddddd2222ddddddddddddddddd");
+	    	List avg=(List)vector2.get(1);
+	    	session.setAttribute("avg", avg);
+	    }
+	    else if(vector2.size()==1)
+	    {
+	    	past =(List)vector2.get(0);
+	    }
+	    
+	    else if(vector3.size()==2)
+	    {System.out.println("ddddddddd3333dddddddddddddddddddd");
+	    	List avg=(List)vector3.get(1);
+	    	session.setAttribute("avg", avg);
+	    }
+	    else if(vector3.size()==1)
+	    {
+	    	rest =(List)vector3.get(0);
+	    }
+	    else{session.setAttribute("avg", a);}
+	    
 		
-
+	    
 		session.setAttribute("list", list);
 		session.setAttribute("past", past);
 		session.setAttribute("rest", rest);
